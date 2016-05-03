412957e4c8078a4ac8dad1c6a7330eb3d501a9cb
Mathieu dhainaut <mathieu.dhainaut@gmail.com>
2016-05-04 01:31:57 +0200

    update orientation from Quaternion algorithm

--- js/osh/osh-leaflet-datamarker.js
+++ js/osh/osh-leaflet-datamarker.js
@@ -117,97 +117,34 @@ OSH.LeafletDataMarker.prototype.onUpdateLocationData = function(data) {
  * Callback after receiving orientation values
  */ 
 OSH.LeafletDataMarker.prototype.onUpdateOrientationData = function(data) {
-    /*var rec = String.fromCharCode.apply(null, new Uint8Array(data));
+    var rec = String.fromCharCode.apply(null, new Uint8Array(data));
     var tokens = rec.trim().split(",");
     var qx = parseFloat(tokens[1]);
     var qy = parseFloat(tokens[2]);
     var qz = parseFloat(tokens[3]);
     var qw = parseFloat(tokens[4]);
-    
-    //normalize
-    var norm = (qx * qx + qy * qy + qz * qz + qw * qw);
 
-    if (norm > 0.0) {
-        norm = 1.0 / Math.sqrt(norm);
-        qx = norm * qx;
-        qy = norm * qy;
-        qz = norm * qz;
-        qw = norm * qw;
-    } else {
-      qx = 0.0;
-      qy = 0.0;
-      qz = 0.0;
-      qw = 0.0;
-    }
+    //var q = new THREE.Quaternion(qx, qy, qz, qw);
+    //var look = new THREE.Vector3( 0, 0, -1 );
+    //look.applyQuaternion(q);
     
-    //rotate
-    var v0 = 0;
-    var v1 = 1;
-    var v2 = 0;
+    // look dir vector
+    var x = 0;
+    var y = 0;
+    var z = -1;
+
+    // calculate quat * vector
+    var ix =  qw * x + qy * z - qz * y;
+    var iy =  qw * y + qz * x - qx * z;
+    var iz =  qw * z + qx * y - qy * x;
+    var iw = - qx * x - qy * y - qz * z;
+
+    // calculate result * inverse quat
+    xp = ix * qw + iw * - qx + iy * - qz - iz * - qy;
+    yp = iy * qw + iw * - qy + iz * - qx - ix * - qz;
+    zp = iz * qw + iw * - qz + ix * - qy - iy * - qx;
     
-    var twoxx = 2.0 * qx * qx;
-    var twoyy = 2.0 * qy * qy;
-    var twozz = 2.0 * qz * qz;
-    
-    var xy = qx * qy;
-    var yz = qy * qz;
-    var xz = qx * qz;
-    var wx = qw * qx;
-    var wy = qw * qy;
-    var wz = qw * qz;
-    
-    var vx = v0 * (1.0 - twoyy - twozz) +
-               v1 * (2.0 * (xy - wz)) +
-               v2 * (2.0 * (xz + wy));
-    
-    var vy = v0 * (2.0 * (xy + wz)) +
-               v1 * (1.0 - twoxx - twozz) +
-               v2 * (2.0 * (yz - wx));
-    
-    var vz = v0 * (2.0 * (xz - wy)) +
-               v1 * (2.0 * (yz + wx)) +
-               v2 * (1.0 - twoxx - twoyy);
-    
-            
-    var heading = 90. - ((Math.atan2(vy, vx)) * 180 / Math.PI); 
-    heading = heading  * 180 / Math.PI;
-    if (heading > 180.)
-        heading -= 360.;
-        
-    self.marker.setRotationAngle(heading);*/
-    
-    var rec = String.fromCharCode.apply(null, new Uint8Array(data));
-    var tokens = rec.trim().split(",");
-    var qw = parseFloat(tokens[1]);
-    var qx = parseFloat(tokens[2]);
-    var qy = parseFloat(tokens[3]);
-    var qz = parseFloat(tokens[4]);
-    
-    //from https://android.googlesource.com/platform/external/jmonkeyengine/+/master/engine/src/core/com/jme3/math/Quaternion.java
-    var angles = [3];
-    
-    var sqw = qw * qw;
-    var sqx = qx * qx;
-    var sqy = qy * qy;
-    var sqz = qz * qz;
-    var unit = sqx + sqy + sqz + sqw; // if normalized is one, otherwise
-    // is correction factor
-    var test = qx * qy + qz * qw;
-    if (test > 0.499 * unit) { // singularity at north pole
-        angles[1] = 2 * Math.atan2(qx, qw);
-        angles[2] = Math.PI/2;
-        angles[0] = 0;
-    } else if (test < -0.499 * unit) { // singularity at south pole
-        angles[1] = -2 * Math.atan2(qx, qw);
-        angles[2] = -Math.PI/2;
-        angles[0] = 0;
-    } else {
-        angles[1] = Math.atan2(2 * qy * qw - 2 * qx * qz, sqx - sqy - sqz + sqw); // roll or heading 
-        angles[2] = Math.asin(2 * test / unit); // pitch or attitude
-        angles[0] = Math.atan2(2 * qx * qw - 2 * qy * qz, -sqx + sqy - sqz + sqw); // yaw or bank
-    }
-        
-    yaw = angles[0] * 180 / Math.PI;
-    this.marker.setRotationAngle(yaw);
-    
+    var yaw = 90 - (180/Math.PI*Math.atan2(yp, xp));
+
+    this.marker.setRotationAngle(yaw);    
 };
